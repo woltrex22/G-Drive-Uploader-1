df1c0fdcd878d46b171b591ef4ae59739ab67747
woltrex22 <88374937+woltrex22@users.noreply.github.com>
2021-08-29 16:30:32 +0530

    Update TEXT.py

--- plugins/TEXT.py
+++ plugins/TEXT.py
@@ -52,12 +52,12 @@ DOWN_TWO = True
 DOWNLOAD = "Downloading Started ..."
 DOWN_MEGA = "Downloading Started..."
 DOWN_COMPLETE = "Downloading complete !!"
-NOT_AUTH = "You Are Not Authorised To Using this Bot \n\n Please Authorise Me Using /auth  \n\n @aryanvikash"
-REVOKE_FAIL = "You Are Already UnAuthorised \n. Please Use /auth To Authorise \n\n report At @aryanvikash "
+NOT_AUTH = "You Are Not Authorised To Using this Bot \n\n Please Authorise Me Using /auth"
+REVOKE_FAIL = "You Are Already UnAuthorised \n. Please Use /auth To Authorise"
 AUTH_SUCC = "Authorised Successfully  !! \n\n Now Send me A direct Link :)"
 ALREADY_AUTH = "You Are Already Authorised ! \n\n Wanna Change Drive Account? \n\n Use /revoke \n\nYou want your own bot? \n Watch this <b><a href="">Tutorial</a></b> Video "
 AUTH_URL = 'Generate And Copy Your Google Drive Token And Send It To Me \n 👇 \n <a href ="{}">Get Token</a> \n You want your own bot? \n
-   Watch this <b><a href="">Tutorial</a></b> Video'
+   Watch this <b><a href="https://www.google.com">Tutorial</a></b> Video'
 UPLOADING = "Download Complete !! \n Uploading Your file"
 REVOKE_TOK = " Your Token is Revoked Successfully !! \n\n Use /auth To Re-Authorise Your Drive Acc. "
 # DOWN_PATH = "Downloads\\" #windows path
