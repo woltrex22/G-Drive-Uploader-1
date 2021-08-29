e997c559ba0bfe98fff310e2fe8b6fb82200f278
woltrex22 <88374937+woltrex22@users.noreply.github.com>
2021-08-29 17:00:43 +0530

    Update TEXT.py

--- plugins/TEXT.py
+++ plugins/TEXT.py
@@ -66,15 +66,15 @@ REVOKE_TOK = " Your Token is Revoked Successfully !! \n\n Use /auth To Re-Author
 # DOWN_PATH = "Downloads\\" #windows path
 DOWN_PATH = "Downloads/"  # Linux path
 DOWNLOAD_URL = "Your File Uploaded Successfully \n\n <b>Filename</b> : {} \n\n <b> Size</b> : {} MB \n\n <b>Download</b> {}"
-AUTH_ERROR = '''
+AUTH_ERROR = """
 AUTH Error !! 
 Please  Send Me a  valid Token 
            or 
 Re - Authorise Me /auth
 
 <b>You want your own bot?</b>
-Watch this <a href="https://www.google.com">Tutorial</a> Video
-'''
+Watch this <code><a href="https://www.google.com">Tutorial</a></code> Video
+"""
 OPENLOAD = True
 DROPBOX = True
 MEGA = True
